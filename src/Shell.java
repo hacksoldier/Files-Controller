5ade05395659ce9f7d6634cf4281e31e36da23b9
Marco Velluto <marco.velluto@csttech.it>
2016-05-31 10:37:09 +0200

    add shell

---
+++ src/Shell.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2016 Marco Velluto. All rights reserved.
+ */
+
+/**
+ * @author Marco Velluto
+ * @since Java 1.8
+ */
+public class Shell
+{
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        
+        try
+        {
+
+        }
+        catch (Exception e)
+        {
+            // TODO: handle exception
+        }
+        // TODO Auto-generated method stub
+
+    }
+
+}
